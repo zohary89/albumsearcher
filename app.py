761ef017233a43e12b78699cc5e8b6d1e1f60f56
zohary89 <71074080+zohary89@users.noreply.github.com>
2020-10-24 15:44:30 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, redirect, render_template, request, url_for
 import requests
 
 app = Flask(__name__)
 
-@app.route('/', methods =['GET'])
+
+@app.route('/', methods=['GET'])
 def index():
     is_valid = request.args.get('valid', default='true') == 'true'
     return render_template('index.html', is_valid=is_valid)
 
 
-@app.route('/albums', methods =['GET'])
+@app.route('/albums', methods=['GET'])
 def albums():
     artist_name = request.args.get('artist').title()
     albums_resp = requests.get(f'http://theaudiodb.com/api/v1/json/1/searchalbum.php?s={artist_name}')
@@ -17,35 +18,35 @@ def albums():
     artist_resp = requests.get(f'http://theaudiodb.com/api/v1/json/1/search.php?s={artist_name}')
     artist_resp_json = artist_resp.json()
     if albums_resp_json['album'] is None:
-      return redirect(url_for('index', valid='false'))
+        return redirect(url_for('index', valid='false'))
     albums = albums_resp_json['album']
     artist = artist_resp_json['artists']
     sorted_albums = sorted(albums, key=lambda k: k["intYearReleased"], reverse=True) 
     return render_template(
-      'results.html',
-      ARTIST_NAME=artist_name,
-      all_albums=sorted_albums,
-      artist_info=artist,
+        'results.html', 
+        ARTIST_NAME=artist_name, 
+        all_albums=sorted_albums, 
+        artist_info=artist,
     )
 
 
 @app.route('/albums/<album_id>')
 def album(album_id):
-  album_resp = requests.get(f'https://theaudiodb.com/api/v1/json/1/album.php?m={album_id}')
-  tracks_resp = requests.get(f'https://theaudiodb.com/api/v1/json/1/track.php?m={album_id}')
-  album_resp_json = album_resp.json()
-  tracks_resp_json = tracks_resp.json()
-  if album_resp_json['album'] is None:
-     return redirect(url_for('index'))
-  album_info = album_resp_json['album'][0]
-  tracks_info = tracks_resp_json['track']
-  return render_template(
-      'album.html',
-      album_info=album_info,
-      tracks_info=tracks_info
+    album_resp = requests.get(f'https://theaudiodb.com/api/v1/json/1/album.php?m={album_id}')
+    tracks_resp = requests.get(f'https://theaudiodb.com/api/v1/json/1/track.php?m={album_id}')
+    album_resp_json = album_resp.json()
+    tracks_resp_json = tracks_resp.json()
+    if album_resp_json['album'] is None:
+        return redirect(url_for('index'))
+    album_info = album_resp_json['album'][0]
+    tracks_info = tracks_resp_json['track']
+    return render_template(
+        'album.html',
+        album_info=album_info,
+        tracks_info=tracks_info
     )
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 
